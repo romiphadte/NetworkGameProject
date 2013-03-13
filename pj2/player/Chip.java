cd959cac12aba48458d0e55b84f66e089bdbc69e
Romi Phadte <romiphadte@gmail.com>
2013-03-13 01:04:27 -0700

    added new file chip`

---
+++ pj2/player/Chip.java
@@ -0,0 +1,46 @@
+/*Chip.java*/
+
+package player
+
+class Chip{
+    //makes a chip with no location
+    //private int color;
+    //private int x;
+    //private int y;
+    //public Chip[] networkedTo;
+   
+
+    public void Chip(){
+        x=-1;
+        y=-1;
+        color=-1;
+    }
+    
+    public void Chip(int X, int Y, int Color){
+    //x=X;
+    //y=Y;
+    //color=Color;
+    }
+    public int getX(){
+    //    return x;
+    }
+
+    public int getY(){
+    //    return y;
+    }
+
+    public int color(){
+        return color;
+    }
+
+    public void remove(){
+        x=-1;
+        y=-1;
+        color=-1;
+        //Add code that goes through all nodes in networkedTo and deletes this chip from their lists.
+    }
+}
+
+
+        
+
