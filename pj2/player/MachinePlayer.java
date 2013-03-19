d795d65800f7baf51369173e787edaa5e3085165
Nicholas Sun <olassun2@gmail.com>
2013-03-18 21:16:42 -0700

    majority basic functionality implemented

--- pj2/player/MachinePlayer.java
+++ pj2/player/MachinePlayer.java
@@ -3,50 +3,63 @@
 package player;
 
 /**
- *  An implementation of an automatic Network player.  Keeps track of moves
- *  made by both players.  Can select a move for itself.
- */
+*  An implementation of an automatic Network player.  Keeps track of moves
+*  made by both players.  Can select a move for itself.
+*/
 public class MachinePlayer extends Player {
 
-  protected Board gameboard;
-  protected int color;
-  protected int searchDepth;
+    public static final int WHITE = 1;
+    public static final int BLACK = 0;
+    protected Board gameboard;
+    protected int color;
+    protected int searchDepth;
 
-  // Creates a machine player with the given color.  Color is either 0 (black)
-  // or 1 (white).  (White has the first move.)
-  public MachinePlayer(int color) {
-  }
+    // Creates a machine player with the given color.  Color is either 0 (black)
+    // or 1 (white).  (White has the first move.)
+    public MachinePlayer(int color) {
+        this.color = color;
+        gameboard = new Board();
 
-  // Creates a machine player with the given color and search depth.  Color is
-  // either 0 (black) or 1 (white).  (White has the first move.)
-  public MachinePlayer(int color, int searchDepth) {
-  }
+    }
 
-  // Returns a new move by "this" player.  Internally records the move (updates
-  // the internal game board) as a move by "this" player.
-  public Move chooseMove() {
-    return new Move();
-  } 
+    // Creates a machine player with the given color and search depth.  Color is
+    // either 0 (black) or 1 (white).  (White has the first move.)
+    public MachinePlayer(int color, int searchDepth) {
+        this(color);
+        this.searchDepth = searchDepth;
+    }
 
-  // If the Move m is legal, records the move as a move by the opponent
-  // (updates the internal game board) and returns true.  If the move is
-  // illegal, returns false without modifying the internal state of "this"
-  // player.  This method allows your opponents to inform you of their moves.
-  public boolean opponentMove(Move m) {
-    return false;
-  }
+    // Returns a new move by "this" player.  Internally records the move (updates
+    // the internal game board) as a move by "this" player.
+    public Move chooseMove() {
+        return new Move();
+    } 
 
-  // If the Move m is legal, records the move as a move by "this" player
-  // (updates the internal game board) and returns true.  If the move is
-  // illegal, returns false without modifying the internal state of "this"
-  // player.  This method is used to help set up "Network problems" for your
-  // player to solve.
-  public boolean forceMove(Move m) {
-    return false;
-  }
+    // If the Move m is legal, records the move as a move by the opponent
+    // (updates the internal game board) and returns true.  If the move is
+    // illegal, returns false without modifying the internal state of "this"
+    // player.  This method allows your opponents to inform you of their moves.
+    public boolean opponentMove(Move m) {
+        int opposingColor;
+        if (color == WHITE) {
+            opposingColor = BLACK;
+        } else {
+            opposingColor = WHITE;
+        }
+        return gameboard.makeMove(opposingColor, m);
+    }
 
-  private Move bestMove(Board board, int searchDepth) {
-    return new Move();
-  }
+    // If the Move m is legal, records the move as a move by "this" player
+    // (updates the internal game board) and returns true.  If the move is
+    // illegal, returns false without modifying the internal state of "this"
+    // player.  This method is used to help set up "Network problems" for your
+    // player to solve.
+    public boolean forceMove(Move m) {
+        return gameboard.makeMove(color, m);
+    }
+
+    private Move bestMove(Board board, int searchDepth) {
+        return new Move();
+    }
 
 }
