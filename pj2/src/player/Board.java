3103aadd16f24cb0965944d0ce5277bc2a4f4713
Nicholas Sun <olassun2@gmail.com>
2013-04-05 19:35:09 -0700

    fixed findNetwork (partially)

--- pj2/src/player/Board.java
+++ pj2/src/player/Board.java
@@ -448,7 +448,7 @@ class Board {
 				if (chip != null && chip.color() == color) {
 					DList net = chip.network(color);
 					DList validNet = validNetworks(net, color);
-					mergeNetworks(networks, net);
+					mergeNetworks(networks, validNet);
 				}
 			}
 		}
